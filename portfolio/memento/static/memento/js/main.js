dfa89eb460ff2d1a19a00f56874ad59599c70ff4
Barissa-Imran <imranbarissa@gmail.com>
2021-09-07 00:01:51 +0300

    add mobile menu styles

--- portfolio/memento/static/memento/js/main.js
+++ portfolio/memento/static/memento/js/main.js
@@ -1,21 +1,25 @@
+// menu settings (mobile nav)
+function menuToggle() {
+    const nav = document.getElementById('menu-overlay');
+    nav.classList.toggle('active');
+    const nav = document.getElementById('toggleIcon');
+    nav.classList.toggle('active');
+}
+
 // change to dark mode or light mode
-// const aTags = document.querySelector('a');
-const navTags = document.querySelector('nav');
-const body = document.querySelector('body');
 const toggle = document.getElementById('toggle');
-const card = document.getElementsByClassName('desc');
 
 toggle.onclick = function() {
     toggle.classList.toggle('active');
+}
+
+// dark mode settings
+const body = document.querySelector('body');
+const navTags = document.querySelector('nav');
+const card = document.getElementsByClassName('desc');
+
+toggle.onclick = function() {
     body.classList.toggle('active');
     navTags.classList.toggle('active');
     card.classList.toggle('active');
 }
-
-// menu settings
-function menuToggle() {
-    const nav = document.getElementById('menu-overlay');
-    nav.classList.toggle('active');
-}
-
-// dark mode settings
