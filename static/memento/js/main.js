1ee3468b1f9f8c8b7a0409d62cf27242b59820ce
Barissa-Imran <imranbarissa@gmail.com>
2021-09-08 02:00:06 +0300

    Add Portfolio styles
    added styling for the portfolio section. made the website responsive from top till portfolio. Migrations to add code fields in project class

--- static/memento/js/main.js
+++ static/memento/js/main.js
@@ -1,18 +1,25 @@
-// change to dark mode or light mode
-// const aTags = document.querySelector('a');
-const navTags = document.querySelector('nav');
-const body = document.querySelector('body');
-const toggle = document.getElementById('toggle');
-const card = document.getElementsByClassName('desc');
-
-toggle.onclick = function() {
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
-    navTags.classList.toggle('active');
-    card.classList.toggle('active');
-}
-// menu settings
+// menu settings (mobile nav)
 function menuToggle() {
     const nav = document.getElementById('menu-overlay');
     nav.classList.toggle('active');
+    const nav = document.getElementById('toggleIcon');
+    nav.classList.toggle('active');
+}
+
+// change to dark mode or light mode
+const toggle = document.getElementById('toggle');
+
+toggle.onclick = function() {
+    toggle.classList.toggle('active');
+}
+
+// dark mode settings
+const body = document.querySelector('body');
+const navTags = document.querySelector('nav');
+const card = document.getElementsByClassName('desc');
+
+toggle.onclick = function() {
+    body.classList.toggle('active');
+    navTags.classList.toggle('active');
+    card.classList.toggle('active');
 }
